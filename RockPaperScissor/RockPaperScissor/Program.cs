43e570d62612e9eff55dc9d5bd40bca73e9c50e9
Sadri TBA <sadri_albaner@hotmail.com>
2017-02-02 14:33:34 +0100

    OOP

--- RockPaperScissor/RockPaperScissor/Program.cs
+++ RockPaperScissor/RockPaperScissor/Program.cs
@@ -11,20 +11,16 @@ namespace RockPaperScissor
     {
         static void Main(string[] args)
         {
+            controller con = new controller();
+
             // 1 = rock
             // 2 = paper
             // 3 = scizor
 
-            Random rand = new Random();
-            model mdl = new model();
-
             while (true)
             {
-
                 Console.WriteLine("1. Login \n 2. Start game \n 3. Terminate game");
                 int val = int.Parse(Console.ReadLine());
-
-
                 if (val == 1)
                 {
                     Console.WriteLine("Angiv navn på første spiller:");
@@ -32,68 +28,18 @@ namespace RockPaperScissor
                     Console.WriteLine("Angiv navn på anden spiller:");
                     string name2 = Console.ReadLine();
 
-                    mdl.name1 = name1;
-                    mdl.name2 = name2;
+                    con.SetNames(name1, name2);
                 }
                 else if (val == 2)
                 {
-                    for (int i = 1; i < 5; i++)
-                    {
-                        if (mdl.name1 != "" && mdl.name2 != "")
-                        {
-                            mdl.val1 = rand.Next(1, 3);
-                            mdl.val2 = rand.Next(1, 3);
-
-                            if (mdl.val1 == 1 && mdl.val2 == 2)
-                            {
-                                mdl.points2 += 1;
-                                Console.WriteLine("Player: " + mdl.name1);
-                            }
-                            else if (mdl.val1 == 1 && mdl.val2 == 3)
-                            {
-                                mdl.points1 += 1;
-                            }
-                            else if (mdl.val1 == 2 && mdl.val2 == 1)
-                            {
-                                mdl.points1 += 1;
-                            }
-                            else if (mdl.val1 == 2 && mdl.val2 == 3)
-                            {
-                                mdl.points2 += 1;
-                            }
-                            else if (mdl.val1 == 3 && mdl.val2 == 1)
-                            {
-                                mdl.points2 += 1;
-                            }
-                            else if (mdl.val1 == 3 && mdl.val2 == 2)
-                            {
-                                mdl.points1 += 1;
-                            }
-                            else
-                            {
-                                Console.WriteLine("DRAW!");
-                                Console.ReadKey();
-                            }
-
-                            Console.WriteLine(mdl.name1 + " has: " + mdl.points1);
-                            Console.WriteLine(mdl.name2 + " has: " + mdl.points2);
-                            Console.ReadKey();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please choose 1. option first!");
-                            Console.ReadKey();
-                        }
-
-
-                    }
+                    con.Play();
                 }
+
                 else
                 {
-                    Console.WriteLine("Du skal først angive navn på spillere.");
+                    Environment.Exit(0);
                 }
             }
-
         }
     }
 }
--- RockPaperScissor/RockPaperScissor/controller.cs
+++ RockPaperScissor/RockPaperScissor/controller.cs
@@ -8,6 +8,71 @@ namespace RockPaperScissor
 {
     class controller
     {
+        model mdl = new model();
+        Random rand = new Random();
 
+        public void SetNames(string name1, string name2)
+        {
+            mdl.name1 = name1;
+            mdl.name2 = name2;
+        }
+
+        public void Play()
+        {
+            mdl.points1 = 0;
+            mdl.points2 = 0;
+
+            if (string.IsNullOrEmpty(mdl.name1) && string.IsNullOrEmpty(mdl.name2))
+            {
+                Console.WriteLine("Angiv venligst navn på begge deltagere!");
+                Console.ReadKey();
+            }
+            else
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    mdl.val1 = rand.Next(1, 3);
+                    mdl.val2 = rand.Next(1, 3);
+                    string draw = "";
+
+                    if (mdl.val1 == 1 && mdl.val2 == 2)
+                    {
+                        mdl.points2 += 1;
+                    }
+                    else if (mdl.val1 == 1 && mdl.val2 == 3)
+                    {
+                        mdl.points1 += 1;
+                    }
+                    else if (mdl.val1 == 2 && mdl.val2 == 1)
+                    {
+                        mdl.points1 += 1;
+                    }
+                    else if (mdl.val1 == 2 && mdl.val2 == 3)
+                    {
+                        mdl.points2 += 1;
+                    }
+                    else if (mdl.val1 == 3 && mdl.val2 == 1)
+                    {
+                        mdl.points2 += 1;
+                    }
+                    else if (mdl.val1 == 3 && mdl.val2 == 2)
+                    {
+                        mdl.points1 += 1;
+                    }
+                    else
+                    {
+                        mdl.points1 += 0;
+                        mdl.points2 += 0;
+                        draw = " uafgjort";
+                    }
+
+                    Console.WriteLine("Runde: " + i + draw);
+                    Console.WriteLine(mdl.name1 + " har: " + mdl.points1);
+                    Console.WriteLine(mdl.name2 + " har: " + mdl.points2 + "\n");
+
+                }
+                Console.ReadKey();
+            }
+        }
     }
 }
